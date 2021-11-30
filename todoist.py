3c501469a3cf670017e0ef63c7369aab698343c7
Adam Reese <areese@gmail.com>
2021-11-30 15:32:06 -0700

    added some methods to api wrapper in todoist.py

--- todoist.py
+++ todoist.py
@@ -6,10 +6,11 @@ from cred_manage.bitwarden import make_bitwarden_container
 import json
 import requests 
 import os
+import sys
 
 def make_todoist_api(config_file='config.json'):
     """
-    A factory method to init a TodoistAPI object by reading the API key from a bitwarden vault.
+    A factory function to init a TodoistAPI object by reading the API key from a bitwarden vault.
     The GUID (Not to be confused with the API key itself) for the BW Vault item is read from 
     a config file
 
@@ -54,16 +55,31 @@ class TodoistAPI():
         # Handle the base URL
         self.base_url = "https://api.todoist.com/rest/v1/" # method endpoints get concatendated onto this
 
-    def _do_get(self, url:str, headers:dict={}, params:dict={}):
+    def _do_get(self, url:str, headers:dict={}, params:dict={}, expected_status_code=200):
         """
-        Does a GET request to the API and returns the result.  That's it
-        The caller should to subsequent processing on the status code
+        Does a GET request to the API and returns the result.  That's it.
+        The caller should to subsequent processing.
 
         Args:
             url ([string]): The URL to do the GET request on
             headers (dict, optional): Any headers that need to be passed along.  We'll update() in auth as needed. Defaults to {}.
             parameters (dict, optional):  A dictionary of URL parameters to be passed along
+            expected_status_code (int or list of int, optional): An integer or list of integers corresponding with the http request code that we would expect to get back
+                200 is the default which means ok
+                204, might also be acceptable when updating a task, for example
+                See:  https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+                See:  https://developer.todoist.com/rest/v1/#overview
         """
+
+        # Handle the expected response(s)
+        err_msg = f"The expected_response argument should be an integer or list of integers corresponding with http status codes.  \nSee:  https://en.wikipedia.org/wiki/List_of_HTTP_status_codes\nSee:  https://developer.todoist.com/rest/v1/#overview"
+        if type(expected_status_code) not in [int, list]:
+            raise ValueError(err_msg)
+        if type(expected_status_code) is not list:
+            expected_status_code = [expected_status_code]
+        for c in expected_status_code:
+            if type(c) is not int:
+                raise ValueError(err_msg)
 
         # Merge Authorization into headers object as needed
         if not headers:
@@ -75,12 +91,13 @@ class TodoistAPI():
         result = requests.get(url=url, headers=headers, params=params)
 
         # Raise and exception if something bad happened
-        if not requests.codes.ok:
+        status_code = result.status_code
+        if status_code not in expected_status_code:
             result.raise_for_status()
 
         return result
     
-    def get_projects(self):
+    def get_all_projects(self):
         """
         Returns the projects under the todoist account.  
         See:  https://developer.todoist.com/rest/v1/#get-a-user-39-s-projects
@@ -95,22 +112,111 @@ class TodoistAPI():
 
         return projects
 
+    def get_active_tasks(self, project_id:int=None, section_id:int=None, label_id:int=None, filter:str=None, lang:str=None, ids:list=None):
+        """
+        Returns tasks, which may be filtered using one or more of the parameters as documented here: 
+            https://developer.todoist.com/rest/v1/?shell#get-active-tasks
 
+        Args:
+            project_id (int, optional): Filters Tasks by Project ID. Defaults to None.
+            section_id (int, optional): Filters Tasks by section ID. Defaults to None.
+            label_id (int, optional): Filters Tasks by Label ID. Defaults to None.
+            filter (str, optional): Filters tasks by any supported filter. Defaults to None.  
+                See:  https://todoist.com/help/articles/introduction-to-filters
+            lang (str, optional): IETF language tag defining what language filter is written in, if differs from default English. Defaults to None.
+            ids (list, optional): 	A list of the task IDs to retrieve, this should be a comma separated list. Defaults to None.
+        """
 
+        # Validate Project ID
+        if project_id is not None and type(project_id) is not int:
+            raise ValueError(f"project_id, if supplied must be an integer.  Got {type(project_id)}.  See:  https://developer.todoist.com/rest/v1/?shell#get-active-tasks")
 
+        # Validate Section ID
+        if section_id is not None and type(section_id) is not int:
+            raise ValueError(f"section_id, if supplied must be an integer.  Got {type(section_id)}.  See:  https://developer.todoist.com/rest/v1/?shell#get-active-tasks")
 
-        print("!")
+        # Validate Label ID
+        if label_id is not None and type(label_id) is not int:
+            raise ValueError(f"label_id, if supplied must be an integer.  Got {type(label_id)}.  See:  https://developer.todoist.com/rest/v1/?shell#get-active-tasks")
 
-        
-        
+        # Validate Filter.  We'll leave most of the rest of the validation up to the API.
+        if filter is not None and type(filter) is not str:
+            raise ValueError(f"filter, if supplied must be a string.  Got {type(filter)}.  See:  https://developer.todoist.com/rest/v1/?shell#get-active-tasks")
 
+        # Validate Lang.  We'll leave most of the rest of the validation up to the API.
+        if lang is not None and type(lang) is not str:
+            raise ValueError(f"lang, if supplied must be a string.  Got {type(lang)}.  See:  https://developer.todoist.com/rest/v1/?shell#get-active-tasks")
 
+        # Validate IDs
+        if ids is not None and type(ids) is not list:
+            raise ValueError(f"ids, if supplied but be a list of integers.  Got {type(ids)}.  See:  https://developer.todoist.com/rest/v1/?shell#get-active-tasks")
+        if type(ids) is list:
+            if len(ids) >=1:
+                for i in ids:
+                    if type(i) is not int:
+                        raise ValueError(f"ids, if supplied must be a list of integers.  Encountered the value {i} which is type {type(i)}.  See:  https://developer.todoist.com/rest/v1/?shell#get-active-tasks")
+            else:
+                print(f"Warning:  An empty list was passed in for the 'ids' argument.  Coercing to None.", file=sys.stderr)
+                ids = None
 
-        
+        # Construct a dict of truthy arguments
+        params = {}
+        if project_id is not None:
+            params['project_id'] = project_id
+        if section_id is not None:
+            params['section_id'] = section_id
+        if label_id is not None:
+            params['label_id'] = label_id
+        if filter is not None:
+            params['filter'] = filter
+        if lang is not None:
+            params['lang'] = lang
+        if ids is not None:
+            params['ids'] = ids
 
-        print("!")
+        # Prepare the request
+        method = "tasks"
+        endpoint_url = f"{self.base_url}{method}"
+
+        # Submit the request
+        result = self._do_get(url=endpoint_url, params=params)
+
+        # Return the results.
+        tasks = result.json()
+        return tasks
+
+    def get_all_tasks(self):
+        """
+        A wrapper method, for convenience around get_active_tasks(), and provides no arguments.  
+        The returned payload then, will simply be all active tasks
+        """
+
+        all_tasks = self.get_active_tasks() # No args means no filtering will take place
+        return all_tasks
+
+    def get_task_by_id(self, task_id:int):
+        """
+        Returns a single task based on the ID of that task.
+
+        Args:
+            task_id (int): [description]
+        """
+
+        # Validate the data type of the task ID
+        if type(task_id) is not int:
+            raise ValueError(f"task_id argument should be an integer.  Got {type(task_id)}")
+
+        method = "tasks"
+        endpoint_url = f"{self.base_url}{method}/{task_id}"  # Here, we simply suffix the task ID in question
+
+        result = self._do_get(url=endpoint_url)
+        task = result.json()
+
+        return task
 
 
 if __name__ == '__main__':
     td = make_todoist_api()
-    print("!")
+    projects=td.get_all_projects()
+    # print(json.dumps(projects, indent=4))
+    print(json.dumps(td.get_task_by_id(task_id=5374849640999), indent=4))
